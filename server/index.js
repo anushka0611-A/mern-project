da7cd689d42e8f58547c48f3e2152dd145a1242c
anushka0611-A <anushkaagarwal396@gmail.com>
2026-03-06 23:19:22 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://mernprojectclient.onrender.com",
     credentials:true
 }))
 app.use(express.json())
